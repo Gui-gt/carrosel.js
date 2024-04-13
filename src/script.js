4ecf8e77372da95a06904bb3ba417d28156e5bcf
Gui-gt <30755guilherme@gmail.com>
2024-04-12 21:33:34 -0300

    att

--- src/script.js
+++ src/script.js
@@ -12,5 +12,26 @@ function showSlider () {
     slider[currentSlide].classList.add('on')
 }
 
+function nextSlider() {
+    hideslider()
+    if(currentSlide === slider.length - 1){
+        currentSlide = 0
+    }else{
+        currentSlide++
+    }
+    showSlider()
+}
+function prevSlider() {
+    hideslider()
+    if(currentSlide === 0){
+        currentSlide = slider.length + 1
+    }else{
+        currentSlide--
+    }
+    showSlider()
+}
+
 btnnext.addEventListener('click' , () => console.log('clicado'))
 btnprev.addEventListener('click' , () => console.log('clicado'))
+
+console.log(slider)
